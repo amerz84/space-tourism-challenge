14e4926d20540f021ea1882230a970ce75d4e936
Amerz84 <snuesen@gmail.com>
2021-12-15 00:12:16 -0600

    Init commit - navbar pretty much complete, homepage progress

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,34 @@
+import { RouterModule } from '@angular/router';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { DestinationComponent } from './destination/destination.component';
+import { CrewComponent } from './crew/crew.component';
+import { TechnologyComponent } from './technology/technology.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    DestinationComponent,
+    CrewComponent,
+    TechnologyComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'destination', component: DestinationComponent },
+      { path: 'crew', component: CrewComponent },
+      { path: 'technology', component: TechnologyComponent, },
+      { path: '**', component: HomeComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
