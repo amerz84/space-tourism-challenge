8830232237174014aefeeeff17a9b097acbfa46f
Amerz84 <snuesen@gmail.com>
2021-12-20 00:21:33 -0600

    Finished desktop version of technology page

--- src/app/technology/technology.component.ts
+++ src/app/technology/technology.component.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { doc, docData, DocumentData, DocumentReference, Firestore } from '@angular/fire/firestore';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'technology',
@@ -6,10 +8,48 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./technology.component.css']
 })
 export class TechnologyComponent implements OnInit {
+  innerWidth!: number;
+  isMobile!: boolean;
+  technologyId!: string;
+  technology$!: DocumentData;
+  firestoreRef!: DocumentReference<DocumentData> | null;
 
-  constructor() { }
+  constructor(private route: ActivatedRoute, private firestore: Firestore) {}
 
   ngOnInit(): void {
+    this.isMobile = (window.innerWidth < 769);
+
+    document.querySelector('nav-link-icon')?.classList.add('active');
+
+    //Gather id value from route to pass
+    this.route.paramMap.subscribe(params => {
+      if (params.get("itemid") !== null) {
+        this.technologyId = params.get("itemid") || '';
+        this.setFirestoreData();
+      }
+      else this.technologyId = '1';
+      this.setFirestoreData();
+    });
+  }
+
+  setFirestoreData() {
+    this.firestoreRef = doc(this.firestore, `technology/${this.technologyId}`);
+
+    if (this.firestoreRef) {
+      docData(this.firestoreRef).pipe().subscribe(res => {
+        this.technology$ = res;
+      });
+    }
+  }
+
+  @HostListener('window:resize', ['$event'])
+  onResize() {
+    this.innerWidth = window.innerWidth;
+
+    if (window.innerWidth < 769) {
+      this.isMobile = true;
+    }
+    else this.isMobile = false;
   }
 
 }
