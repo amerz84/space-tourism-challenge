3210891e84558e792b99e33c7dcdc2428c826e01
Amerz84 <snuesen@gmail.com>
2021-12-21 23:59:28 -0600

    Build for gh pages

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyAIyBE0or_kjgGGdTC6tz3EnKeXktYdNHE",
+    authDomain: "space-tourism-challenge.firebaseapp.com",  
+    projectId: "space-tourism-challenge",  
+    storageBucket: "space-tourism-challenge.appspot.com",  
+    messagingSenderId: "419804566842",  
+    appId: "1:419804566842:web:84f0bad2790b8d563c2c03"  
+  }
 };
